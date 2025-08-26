033893f72f0d92288e9a484c41b5b87caeb550fc
FullStackDeveloperShubham <shubhamfullstackdeveloper@gmail.com>
2025-08-26 22:31:35 +0530

    Password hash , save into db done.using bcryptjs

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,8 +1,10 @@
+import bcrypt from "bcryptjs"
 import mongoose, { Document, Model, Schema } from "mongoose"
 import type { IUser } from "../Types/user.Types"
 
 interface UserDocument extends IUser, Document { }
 
+// ! Define the schema
 const userSchema = new Schema<UserDocument>({
     UserName: {
         type: String,
@@ -21,6 +23,14 @@ const userSchema = new Schema<UserDocument>({
         trim: true
     }
 }, { timestamps: true })
+
+// ! Hash the Password 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
 const User: Model<UserDocument> = mongoose.models.User || mongoose.model<UserDocument>("User", userSchema)
 export default User
